34cb029d911ef6d8f365f87d71a2662419741af6
Ayberkb33 <ayberk.bulut@ogr.sakarya.edu.tr>
2024-10-30 16:48:11 +0300

    first c++ code

---
+++ hello.py
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int number1, number2, sonuc;
+
+	cout << "ilk sayiyi giriniz";
+	cin >> number1;
+
+	cout << "ikinci sayiyi giriniz";
+	cin >> number2;
+
+	sonuc = number1 + number2;
+
+	cout << "Sonuc esittir= " << sonuc << endl;
+
+
+
+	return 0;
+}
